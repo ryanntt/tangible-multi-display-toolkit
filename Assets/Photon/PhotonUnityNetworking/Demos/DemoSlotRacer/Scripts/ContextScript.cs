06025dc230c51c2fa7ac80909be1e116595e67a1
Ryan Nguyen <ryannguyentt@gmail.com>
2019-03-12 10:22:16 +1100

    Built context indicator that responds to its distance to the car

---
+++ Assets/Photon/PhotonUnityNetworking/Demos/DemoSlotRacer/Scripts/ContextScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextScript : MonoBehaviour
+{
+    public Transform target;
+
+    [SerializeField]
+    private Sprite inactive, activated;
+
+    [SerializeField]
+    private SpriteRenderer statusRenderer;
+
+    [SerializeField]
+    private TextMesh textMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dist = Vector3.Distance(statusRenderer.transform.position, target.transform.position);
+        print("Distance to car is:"+ dist);
+
+        if ( dist <= 3.0) 
+        {
+            ActivateContext();
+            UpdateText("Activated :)");
+        } else
+        {
+            DeactivateContext();
+            UpdateText("Inactive");
+        }
+    }
+
+    // To change the visual of context indicator
+    public void ActivateContext() 
+    {
+        statusRenderer.sprite = activated;
+    }
+
+    public void DeactivateContext() {
+        statusRenderer.sprite = inactive; 
+    }
+
+    // To change the display text
+    public void UpdateText(string textdisplay)
+    {
+        textMesh.text = textdisplay;
+    }
+}
