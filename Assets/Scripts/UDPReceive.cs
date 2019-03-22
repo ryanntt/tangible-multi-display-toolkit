fc1d8e1d2473fb74e1dd003282fb0755e16aa8b2
HoggenMari <marius.hoggenmueller@campus.lmu.de>
2019-03-22 15:10:11 +1100

    * established udp receive and send * established udp receive and send * established udp receive and send * established udp receive and send * established udp receive and send * established udp receive and send * established udp receive and send * established udp receive and send

--- Assets/Scripts/UDPReceive.cs
+++ Assets/Scripts/UDPReceive.cs
@@ -1,72 +1,211 @@
-﻿using System;
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
+
+using System;
+using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using UnityEngine;
+using System.Threading;
 
 public class UDPReceive : MonoBehaviour
 {
-    UdpClient udpClient;
-    IPEndPoint RemoteIpEndPoint;
 
-    //port exposed for unity
-    [SerializeField] int EndPointPort = 6454;
+    // receiving Thread
+    Thread receiveThread;
+    Thread sendThread;
 
-    // Use this for initialization
-    void Start()
+    // udpclient object
+    UdpClient receiveClient;
+    UdpClient sendClient;
+
+    // send data endpoint
+    IPEndPoint remoteEndPoint;
+
+
+    // public
+    public string IP = "192.168.0.103";
+    public int receivePort; // define > init
+    public int sendPort; // define > init
+
+    // infos
+    public string lastReceivedUDPPacket = "";
+    public string allReceivedUDPPackets = ""; // clean up this from time to time!
+
+
+    // start from shell
+    private static void Main()
     {
-        RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, EndPointPort);
+        UDPReceive receiveObj = new UDPReceive();
+        receiveObj.init();
 
-        udpClient = new UdpClient();
-        udpClient.Client.Bind(RemoteIpEndPoint);
-
-        PrepareForUDP();
-    }
-
-
-    //init an asynchronous callback to be called on reciept of a udp packet
-    void PrepareForUDP()
-    {
-        AsyncCallback callback = new AsyncCallback(UDPMsgRecievedCallback);
-        udpClient.BeginReceive(callback, null);
-    }
-
-    /// <summary>
-    /// Function designed to be called when a UDP message is recieved
-    /// if we wanted to listen for the next we might put another 
-    /// 'BeginReceive()' method at the end of this method.
-    /// </summary>
-    /// <param name="result">Result.</param>
-    private void UDPMsgRecievedCallback(IAsyncResult result)
-    {
-        byte[] received = udpClient.EndReceive(result, ref RemoteIpEndPoint);
-
-        int dmx = 18;
-        StringBuilder[] sb = new StringBuilder[21];
-        String[] str = new String[21];
-
-        for (int i = 0; i < 21; i++) {
-            sb[i] = new StringBuilder();
-            sb[i].Append("#");
-            sb[i].Append(received[dmx++].ToString("X2"));
-            sb[i].Append(received[dmx++].ToString("X2"));
-            sb[i].Append(received[dmx++].ToString("X2"));
-            str[i] = sb[i].ToString();
-        }
-
-        /*for (int i=0; i<str.Length; i++)
+        string text = "";
+        do
         {
-            Debug.Log("LEDs: " + str[i]);
+            text = Console.ReadLine();
+        }
+        while (!text.Equals("exit"));
+    }
+    // start from unity3d
+    public void Start()
+    {
+
+        init();
+    }
+
+
+    // init
+    private void init()
+    {
+        // Endpunkt definieren, von dem die Nachrichten gesendet werden.
+        print("UDPSend.init()");
+
+        // define port
+        receivePort = 6454;
+        sendPort = 6455;
+
+        // status
+        //print("Sending to 127.0.0.1 : " + port);
+        //print("Test-Sending to this Port: nc -u 127.0.0.1  " + port + "");
+
+
+        //remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+
+        // ----------------------------
+        // Abhören
+        // ----------------------------
+        // Lokalen Endpunkt definieren (wo Nachrichten empfangen werden).
+        // Einen neuen Thread für den Empfang eingehender Nachrichten erstellen.
+        receiveClient = new UdpClient(receivePort);
+
+        sendClient = new UdpClient(sendPort);
+
+
+        receiveThread = new Thread(
+            new ThreadStart(() => ReceiveData(receiveClient)));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+        sendThread = new Thread(
+            new ThreadStart(() => SendData(sendClient)));
+        sendThread.IsBackground = true;
+        sendThread.Start();
+
+        /*while (true)
+        {
+            string text = "test to ipad";
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            client.Send(data, data.Length, "192.168.0.103", 6454);
         }*/
 
-        //Output the colors received color values to the Car's LEDs
-        LEDControl led = GetComponent<LEDControl>();
-        led.ChangeColor(str);
-
-        PrepareForUDP();
     }
+
+    public void SendData(UdpClient sendClient)
+    {
+        print("Send to iPad");
+
+        while (true)
+        {
+            try
+            {
+                print("Send to iPad");
+                string text = "test to ipad from second screen";
+                byte[] data = Encoding.UTF8.GetBytes(text);
+                sendClient.Send(data, data.Length, "192.168.0.103", sendPort);
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    // receive thread
+    private void ReceiveData(UdpClient client)
+    {
+
+        while (true)
+        {
+
+            try
+            {
+                // Bytes empfangen.
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] received = client.Receive(ref anyIP);
+
+                print(anyIP.Address);
+
+                // Bytes mit der UTF8-Kodierung in das Textformat kodieren.
+                string text = Encoding.UTF8.GetString(received);
+
+                // Den abgerufenen Text anzeigen.
+                print(">> " + text);
+
+                int dmx = 18;
+                StringBuilder[] sb = new StringBuilder[21];
+                String[] str = new String[21];
+
+                for (int i = 0; i < 21; i++)
+                {
+                    sb[i] = new StringBuilder();
+                    sb[i].Append("#");
+                    sb[i].Append(received[dmx++].ToString("X2"));
+                    sb[i].Append(received[dmx++].ToString("X2"));
+                    sb[i].Append(received[dmx++].ToString("X2"));
+                    str[i] = sb[i].ToString();
+                }
+
+                /*for (int i=0; i<str.Length; i++)
+                {
+                    Debug.Log("LEDs: " + str[i]);
+                }*/
+
+                //Output the colors received color values to the Car's LEDs
+                LEDControl led = GetComponent<LEDControl>();
+                led.ChangeColor(str);
+
+                //send();
+                //string t = "test to ipad";
+                //byte[] data = Encoding.UTF8.GetBytes(t);
+                //client.Send(data, data.Length, "192.168.0.103", 6454);
+
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    /*public void SendData(UdpClient client, IPEndPoint remoteEndPoint)
+    {
+        try
+        {
+            string text;
+            do
+            {
+                text = "test to ipad";
+
+                // Den Text zum Remote-Client senden.
+                if (text != "")
+                {
+                    remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+
+                    // Daten mit der UTF8-Kodierung in das Binärformat kodieren.
+                    byte[] data = Encoding.UTF8.GetBytes(text);
+
+                    // Den Text zum Remote-Client senden.
+                    client.Send(data, data.Length, remoteEndPoint);
+
+                    print(">> " + text);
+                }
+            } while (text != "");
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+    }*/
+
 
     private string ExtractString(byte[] packet, int start, int length)
     {
@@ -82,8 +221,7 @@ public class UDPReceive : MonoBehaviour
     private void OnDestroy()
     {
         // make sure to clean up sockets on exit
-        udpClient.Client.Close();
-        udpClient.Close();
+        receiveClient.Client.Close();
+        receiveClient.Close();
     }
-
 }
