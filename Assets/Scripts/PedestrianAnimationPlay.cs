128d23b12000835cf97963f6ec17eb2eca2112fb
Ryan Nguyen <trungthng@gmail.com>
2019-05-25 17:40:35 +1000

    Animated car and pedestrian in shared environment

---
+++ Assets/Scripts/PedestrianAnimationPlay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianAnimationPlay : MonoBehaviour
+{
+    public GameObject[] ContextIndicators;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check which context is activated
+
+
+        // Play the animation according to which context is activated, also according to the car's relative distance to context indicator.
+
+        for (int i = 0; i < this.ContextIndicators.Length; i++)
+        {
+            ContextScript ctxScript = this.ContextIndicators[i].GetComponent<ContextScript>();
+            if (ctxScript.isActivated == true)
+            {
+                if (ctxScript.ID == 1)
+                {
+                    GetComponent<Animator>().Play("HumanoidWalk");
+                }
+                else
+                {
+                    GetComponent<Animator>().Play("HumanoidIddle");
+                }
+            }
+        }
+    }
+}
